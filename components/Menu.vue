c928c873070320eb022fc5c483f2f843a00c9bbb
Taci Öztürk <121730070+taciozturk@users.noreply.github.com>
2024-04-11 10:36:26 +0300

    added seo to index

---
+++ components/Menu.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="font-mono">
+    <ul class="flex space-x-4">
+      <li>
+        <ULink
+          active-class="text-primary"
+          inactive-class="text-gray-600 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200"
+          to="/"
+          >Home</ULink
+        >
+      </li>
+      <li>
+        <ULink
+          active-class="text-primary"
+          inactive-class="text-gray-600 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200"
+          to="/about"
+          >About</ULink
+        >
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
