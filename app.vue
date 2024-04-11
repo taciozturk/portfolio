086db60ea94cef8b555a6ffb0e71bba895f5f27b
Taci Öztürk <121730070+taciozturk@users.noreply.github.com>
2024-04-11 08:07:04 +0300

    added nav links

--- app.vue
+++ app.vue
@@ -1,4 +1,24 @@
 <template>
+  <nav>
+    <ul>
+      <li>
+        <ULink
+          active-class="text-primary"
+          inactive-class="text-gray-600 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200"
+          to="/"
+          >Home</ULink
+        >
+      </li>
+      <li>
+        <ULink
+          active-class="text-primary"
+          inactive-class="text-gray-600 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200"
+          to="/about"
+          >About</ULink
+        >
+      </li>
+    </ul>
+  </nav>
   <div>
     <NuxtPage />
   </div>
---
+++ pages/about.vue
@@ -0,0 +1,3 @@
+<template>
+<h1>This page is about me</h1>
+</template>
